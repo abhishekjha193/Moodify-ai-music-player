6cc8234ee30304b94d6a213f44e6af860dcd5b4d
Abhishek Jha <abhibj2003@gmail.com>
2026-06-01 00:01:14 +0530

    Added new feature

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,23 @@
+import { useContext, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app.routes";
+import { AuthContext } from "./features/auth/auth.context";
 import "./features/shared/styles/global.scss";
 
-function App() {
+function AppContent() {
+  const { loading } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!loading) {
+      document.getElementById("loader")?.remove();
+    }
+  }, [loading]);
+
+  if (loading) {
+    return null;
+  }
+
   return <RouterProvider router={router} />;
 }
 
-export default App;
+export default AppContent;
